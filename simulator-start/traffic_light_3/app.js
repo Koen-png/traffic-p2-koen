85ae8e9b0f7c25a82fe7c0f01102e6fe6761d054
Koen-png <dekoenduin@gmail.com>
2023-12-18 11:56:54 +0100

    opdracht3

---
+++ simulator-start/traffic_light_3/app.js
@@ -0,0 +1,24 @@
+let working
+let outOfOrder
+
+turnOn(){
+    setTimeout(de functie die moet werken, seconde in ms);
+    setTimeout(de functie die moet werken, seconde in ms);
+    setTimeout(de functie die moet werken, seconde in ms);
+
+}
+blink(){
+
+}
+turnOff(){
+
+}
+turnRed(){
+
+}
+turnYellow(){
+
+}
+turnGreen(){
+
+}
