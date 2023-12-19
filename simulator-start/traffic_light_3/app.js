d1cd6ce78be1faa18690c060b9ac0f93b8611b00
Koen-png <dekoenduin@gmail.com>
2023-12-19 13:12:09 +0100

    assignment 4

--- simulator-start/traffic_light_3/app.js
+++ simulator-start/traffic_light_3/app.js
@@ -1,24 +1,41 @@
-let working
-let outOfOrder
+let working;
+let outOfOrder = false;
+let trafficlight = document.getElementById("traffic-light");
 
-turnOn(){
-    setTimeout(de functie die moet werken, seconde in ms);
-    setTimeout(de functie die moet werken, seconde in ms);
-    setTimeout(de functie die moet werken, seconde in ms);
-
-}
-blink(){
-
-}
-turnOff(){
+function turnOn(){
+    clearTimeout(working);
+    working = setTimeout(turnRed, 0);
+    working = setTimeout(turnGreen, 4000);
+    working = setTimeout(turnYellow, 8000);
+    working = setTimeout(turnOn, 10000);
 
 }
-turnRed(){
-
+function blink(){
+    clearTimeout(working);
+    working = setTimeout(turnOff, 500);
+    working = setTimeout(turnYellow, 1000);
+    working = setTimeout(blink, 1500);
 }
-turnYellow(){
 
+function turnOff(){
+    trafficlight.src = "img/shutOff.png"
+    console.log("Off");
 }
-turnGreen(){
 
+function turnOffForreal(){
+    clearTimeout(working);
+    turnOff();
+}
+
+function turnRed(){
+    trafficlight.src = "img/trafficLightRed.png"
+    console.log("Red");
+}
+function turnYellow(){
+    trafficlight.src = "img/trafficLightYellow.png"
+    console.log("Yellow");
+}
+function turnGreen(){
+    trafficlight.src = "img/trafficLightGreen.png"
+    console.log("Green");
 }
